618ccedb6f861ca3c1b565b5ff3e77532696a149
sqwezzy <maks.subbotko46@gmail.com>
2018-10-07 10:41:14 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  var n = primeDivisors(base);
+	var tmp = n;
+	var result = 0;
+	while(number > n){
+		result = result + Math.floor(number / n);
+		n = n * tmp;
+	}
+
+	return result;
+}
+
+function primeDivisors(base){
+	var result = [];
+	var number = base;
+	var curNumber = number;
+	var probe = 2;
+	while(curNumber != 1){
+		if(curNumber % probe != 0){
+			probe++;
+		}
+		else{
+			curNumber = curNumber / probe;
+			result.push(probe);
+		}
+	}
+	var max = result[0];
+	for(var i = 0; i < result.length; i++){
+		if(max < result[i]){
+			max = result[i];
+		}
+	}
+	return max;
 }
